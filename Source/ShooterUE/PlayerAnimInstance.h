7076590b057d44e61ff36db1706a327e70c7ce45
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2023-04-03 17:14:10 -0700

    Empezamos con el AnimationInstan

---
+++ Source/ShooterUE/PlayerAnimInstance.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERUE_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+	
+public:
+	//Funciones
+	//Indicamos que la funcion de abajo puede ser llamada como un nodo dentro de los Blueprint
+	UFUNCTION(BlueprintCallable)	 
+	void UpdateAnimationProperties(float Deltatime);
+	
+	//Con virtual estamos diciendo que vamos a sobreescribir una funcion y a lo ultimo un override
+	virtual void NativeInitializeAnimation() override;
+
+private:
+
+	//hacemos que sean visibles nuestras clases y variables que esten debajo antes del ;, además de poder verlo en los 
+	//blueprints Las clases y variables la asignamos en la categoria Movement e indicamos el meta la cual decimos que 
+	//seran privadas
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement,meta = (AllowPrivateAccess = "true"))
+	
+	//Clases
+	class APlayerCharacter* PlayerCharacter;	//Variable puntero que sera utilizada para poder referenciar al jugador
+
+	//variables
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement,meta = (AllowPrivateAccess = "true"))
+	float Speed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement,meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement,meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+};
